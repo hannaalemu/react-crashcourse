32dd634ad2049a51057b98e2bf6d6672a572218a
hannaalemu <hanna9alemu@outlook.com>
2020-02-24 16:46:41 -0800

    events and two way data binding

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Users from './components/users';
+import AddUser from './components/input';
 
 class App extends Component {
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App" className="container">
        <Users></Users>
+      <AddUser></AddUser>
       </div>
     );
   }

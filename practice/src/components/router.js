49c2b18b7d36591e15f6fe3c96cfc7fe0fc72f3f
hannaalemu <hanna9alemu@outlook.com>
2020-03-09 15:33:54 -0700

    react router and links

---
+++ practice/src/components/router.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import '../App';
+import { BrowserRouter as Router, Route , Link, NavLink} from 'react-router-dom';
+
+
+const User = ({match}) => { 
+    return(
+        <h1>Welcome User {match.params.username}</h1>
+    )
+}
+
+class App extends Component {
+    render() {
+        return(
+            <Router>
+                <div className="App">
+                    <ul>
+
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li> <Link to="/about">About</Link></li>
+                    <li><Link to="/user/hanna">Hanna</Link></li>
+                    </ul>
+
+                    <Route path="/" exact render={
+                        ()=> {
+                            return <h1>Welcome Home</h1>
+                        }
+                    }/>
+                    <Route path="/about" exact render={
+                        ()=> {
+                            return <h1>Welcome About</h1>
+                        }
+                    }/>
+                     <Route path="/user/:username" exact component={User}
+    
+                    />
+                </div>
+
+            </Router>
+            
+        )
+    }
+}
+
+export default App;
